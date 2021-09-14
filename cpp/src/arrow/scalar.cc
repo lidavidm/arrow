20b3ff0659f51ef3ccec50030ed0709ef4d3afd3
David Li <li.davidm96@gmail.com>
2021-09-14 11:31:14 -0400

    TEMP

--- cpp/src/arrow/scalar.cc
+++ cpp/src/arrow/scalar.cc
@@ -599,9 +599,8 @@ Result<std::shared_ptr<Scalar>> DictionaryScalar::GetEncodedValue() const {
 std::shared_ptr<DictionaryScalar> DictionaryScalar::Make(std::shared_ptr<Scalar> index,
                                                          std::shared_ptr<Array> dict) {
   auto type = dictionary(index->type, dict->type());
-  auto is_valid = index->is_valid;
   return std::make_shared<DictionaryScalar>(ValueType{std::move(index), std::move(dict)},
-                                            std::move(type), is_valid);
+                                            std::move(type));
 }
 
 namespace {
