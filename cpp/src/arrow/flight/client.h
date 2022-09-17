ca1ac1e54c9db62df9a5caf35dfc82cf2cfa87a2
David Li <li.davidm96@gmail.com>
2022-09-17 08:59:01 -0400

    GH-16604: [C++][FlightRPC] Add async Flight client

--- cpp/src/arrow/flight/client.h
+++ cpp/src/arrow/flight/client.h
@@ -33,6 +33,7 @@
 #include "arrow/result.h"
 #include "arrow/status.h"
 #include "arrow/util/cancel.h"
+#include "arrow/util/future.h"
 
 #include "arrow/flight/type_fwd.h"
 #include "arrow/flight/types.h"  // IWYU pragma: keep
@@ -239,6 +240,12 @@ class ARROW_FLIGHT_EXPORT FlightClient {
     return DoAction({}, action);
   }
 
+  void DoAction(const FlightCallOptions& options, const Action& action,
+                AsyncListener<Result>* listener);
+  void DoAction(const Action& action, AsyncListener<Result>* listener) {
+    DoAction({}, action, listener);
+  }
+
   ARROW_DEPRECATED("Deprecated in 8.0.0. Use Result-returning overload instead.")
   Status DoAction(const FlightCallOptions& options, const Action& action,
                   std::unique_ptr<ResultStream>* results);
@@ -299,6 +306,17 @@ class ARROW_FLIGHT_EXPORT FlightClient {
   arrow::Result<std::unique_ptr<FlightInfo>> GetFlightInfo(
       const FlightDescriptor& descriptor) {
     return GetFlightInfo({}, descriptor);
+  }
+
+  /// \brief Asynchronous GetFlightInfo.
+  /// \param[in] options Per-RPC options
+  /// \param[in] descriptor the dataset request
+  /// \param[in] listener Callbacks for response and RPC completion
+  void GetFlightInfo(const FlightCallOptions& options, const FlightDescriptor& descriptor,
+                     std::shared_ptr<AsyncListener<FlightInfo>> listener);
+  void GetFlightInfo(const FlightDescriptor& descriptor,
+                     std::shared_ptr<AsyncListener<FlightInfo>> listener) {
+    return GetFlightInfo({}, descriptor, listener);
   }
 
   ARROW_DEPRECATED("Deprecated in 8.0.0. Use Result-returning overload instead.")
@@ -363,6 +381,10 @@ class ARROW_FLIGHT_EXPORT FlightClient {
     return DoGet({}, ticket);
   }
 
+  void DoGet(const FlightCallOptions& options, const Ticket& ticket,
+             IpcListener* listener);
+  void DoGet(const Ticket& ticket, IpcListener* listener) { DoGet({}, ticket, listener); }
+
   ARROW_DEPRECATED("Deprecated in 8.0.0. Use Result-returning overload instead.")
   Status DoGet(const FlightCallOptions& options, const Ticket& ticket,
                std::unique_ptr<FlightStreamReader>* stream);
@@ -398,6 +420,9 @@ class ARROW_FLIGHT_EXPORT FlightClient {
                                    const std::shared_ptr<Schema>& schema) {
     return DoPut({}, descriptor, schema);
   }
+
+  void DoPut(const FlightCallOptions& options, IpcPutter* listener);
+  void DoPut(IpcPutter* listener) { DoPut({}, listener); }
 
   ARROW_DEPRECATED("Deprecated in 8.0.0. Use Result-returning overload instead.")
   Status DoPut(const FlightCallOptions& options, const FlightDescriptor& descriptor,
--- cpp/src/arrow/ipc/reader.cc
+++ cpp/src/arrow/ipc/reader.cc
@@ -1983,6 +1983,10 @@ class StreamDecoder::StreamDecoderImpl : public StreamDecoderInternal {
     return message_decoder_.Consume(std::move(buffer));
   }
 
+  Status Consume(std::unique_ptr<Message> message) {
+    return OnMessageDecoded(std::move(message));
+  }
+
   int64_t next_required_size() const { return message_decoder_.next_required_size(); }
 
  private:
@@ -2000,6 +2004,9 @@ Status StreamDecoder::Consume(const uint8_t* data, int64_t size) {
 }
 Status StreamDecoder::Consume(std::shared_ptr<Buffer> buffer) {
   return impl_->Consume(std::move(buffer));
+}
+Status StreamDecoder::Consume(std::unique_ptr<Message> message) {
+  return impl_->Consume(std::move(message));
 }
 
 std::shared_ptr<Schema> StreamDecoder::schema() const { return impl_->schema(); }
